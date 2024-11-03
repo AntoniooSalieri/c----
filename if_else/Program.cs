8cbc0a3a0dbd28cd25172ccf45919cd279a55546
AsuraVV <antonio.fon.salieri@gmail.com>
2024-11-03 09:04:19 +0200

    if and else added

---
+++ if_else/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("login ");
+string username = Console.ReadLine();
+
+if (username == "AsuraVV")
+    Console.Write("._.");
+else
+    Console.Write("+-+");
